ac4ecb65806a7c820673e620f51aa166f98ef127
kosskas <matipolska637@gmail.com>
2022-04-26 09:39:16 +0200

    test

---
+++ src/pl/edu/pg/eti/ksg/po/Main.java
@@ -0,0 +1,8 @@
+package pl.edu.pg.eti.ksg.po;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Test");
+    }
+}
---
+++ src/pl/edu/pg/eti/ksg/po/silnik/Swiat.java
@@ -0,0 +1,5 @@
+package pl.edu.pg.eti.ksg.po.silnik;
+
+public class Swiat {
+
+}
